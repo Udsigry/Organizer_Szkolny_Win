05729c7cea9f37bbb1d933d0eaf63313cfbfa1bb
Udsigry <udigry@gmail.com>
2013-12-30 18:13:19 +0100

    Wersja 1.1 Ulepszenia

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -44,21 +44,26 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Transparent;
+            this.menuStrip1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.plikToolStripMenuItem,
             this.opcjeToolStripMenuItem,
             this.helpToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(888, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(904, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -117,12 +122,13 @@
             // 
             // statusStrip1
             // 
+            this.statusStrip1.BackColor = System.Drawing.Color.Transparent;
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1,
             this.progress});
             this.statusStrip1.Location = new System.Drawing.Point(0, 379);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(888, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(904, 22);
             this.statusStrip1.SizingGrip = false;
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
@@ -141,6 +147,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Location = new System.Drawing.Point(12, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(89, 13);
@@ -158,7 +165,8 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 41);
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(21, 41);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 6;
@@ -167,19 +175,12 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(275, 22);
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(311, 24);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(35, 13);
             this.label4.TabIndex = 7;
             this.label4.Text = "label4";
-            // 
-            // webBrowser1
-            // 
-            this.webBrowser1.Location = new System.Drawing.Point(278, 39);
-            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
-            this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(598, 337);
-            this.webBrowser1.TabIndex = 8;
             // 
             // button1
             // 
@@ -191,19 +192,66 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Lucida Sans", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(324, 73);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 12);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "label5";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(218, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "< Dzień";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(650, 93);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Dzień >";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Location = new System.Drawing.Point(314, 41);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Dzień";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(888, 401);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.webBrowser1);
-            this.Controls.Add(this.label4);
+            this.BackgroundImage = global::Organizer_Szkolny.Properties.Resources.bg;
+            this.ClientSize = new System.Drawing.Size(904, 401);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
@@ -237,8 +285,11 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ToolStripMenuItem oProgramieToolStripMenuItem;
-        public System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Ini;
 
+
 namespace Organizer_Szkolny
 {
     public partial class Form1 : Form
@@ -19,6 +20,7 @@ namespace Organizer_Szkolny
         public WebClient Download = new WebClient();
         public string currentPath = Directory.GetCurrentDirectory();
         public IniFile settings;
+        public int daynum = (int) new DateTime().DayOfWeek;
 
         public Form1()
         {
@@ -83,17 +85,34 @@ namespace Organizer_Szkolny
             {
                 string rtfText = System.IO.File.ReadAllText(currentPath + @"/pliki/zmiany.txt");
                 label3.Text = rtfText;
+                label6.Text = nazwaDnia();
                 label4.Text = "Plan dla klasy: " + settings.IniReadValue("Plan", "nazwa");
-                if (!File.Exists(currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + ".html"))
+                if (daynum != 0 || daynum != 6)
                 {
-                    Download.DownloadFile(new Uri("http://zs-1.pl/plany/plany/" + settings.IniReadValue("Plan", "kod") + ".html"), currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + ".html");
-                    LoadFiles();
+                    if (!File.Exists(currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + "-" + daynum + ".txt"))
+                    {
+                        Aktualizuj();
+                    }
+                    if (File.Exists(currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + "-" + daynum + ".txt"))
+                    {
+                        string plan = System.IO.File.ReadAllText(currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + "-" + daynum + ".txt");
+                        label5.Text = plan;
+                        ustawWielk();
+                    }
                 }
-                if (File.Exists(currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + ".html"))
+                else
                 {
-                    webBrowser1.Url = new Uri("file:///" + currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + ".html") ;
-                    //webBrowser1.Refresh();
-                    //webBrowser1.Update();
+                    daynum = 1;
+                    if (!File.Exists(currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + "-" + daynum + ".txt"))
+                    {
+                        Aktualizuj();
+                    }
+                    if (File.Exists(currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + "-" + daynum + ".txt"))
+                    {
+                        string plan = System.IO.File.ReadAllText(currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + "-" + daynum + ".txt");
+                        label5.Text = plan;
+                        ustawWielk();
+                    }
                 }
             }
             catch (Exception ex)
@@ -117,9 +136,13 @@ namespace Organizer_Szkolny
             Aktualizuj();
         }
 
-        public void Aktualizuj()
+        public void Aktualizuj()   //pobieranie wszystkich plikow
         {
-            Download.DownloadFile(new Uri("http://zs-1.pl/plany/plany/" + settings.IniReadValue("Plan", "kod") + ".html"), currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + ".html");
+            Download.DownloadFile(new Uri("http://zs-1.pl/android/pliki/" + settings.IniReadValue("Plan", "kod") + "-1.txt"), currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + "-1.txt");
+            Download.DownloadFile(new Uri("http://zs-1.pl/android/pliki/" + settings.IniReadValue("Plan", "kod") + "-2.txt"), currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + "-2.txt");
+            Download.DownloadFile(new Uri("http://zs-1.pl/android/pliki/" + settings.IniReadValue("Plan", "kod") + "-3.txt"), currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + "-3.txt");
+            Download.DownloadFile(new Uri("http://zs-1.pl/android/pliki/" + settings.IniReadValue("Plan", "kod") + "-4.txt"), currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + "-4.txt");
+            Download.DownloadFile(new Uri("http://zs-1.pl/android/pliki/" + settings.IniReadValue("Plan", "kod") + "-5.txt"), currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + "-5.txt");
             Download.DownloadFile(new Uri("http://zs-1.pl/android/pliki/uczniowiepliki.txt"), currentPath + @"/pliki/uczniowiepliki.txt");
             Download.DownloadFile(new Uri("http://zs-1.pl/android/pliki/nauczycielenazwy.txt"), currentPath + @"/pliki/nauczycielenazwy.txt");
             Download.DownloadFile(new Uri("http://zs-1.pl/android/pliki/nauczycielepliki.txt"), currentPath + @"/pliki/nauczycielepliki.txt");
@@ -147,6 +170,124 @@ namespace Organizer_Szkolny
         private void button1_Click(object sender, EventArgs e)
         {
             LoadFiles();
+        }
+
+        private void button2_Click(object sender, EventArgs e)          //gdy chcemy zobaczyc plan na poprzedni dzien tygodnia roboczego
+        {
+            if (daynum == 1)
+            {
+                daynum = 5;
+            }
+            else
+            {
+                daynum--;
+            }
+            label6.Text = nazwaDnia();
+            if (!File.Exists(currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + "-" + daynum + ".txt"))
+            {
+                Aktualizuj();
+            }
+            if (File.Exists(currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + "-" + daynum + ".txt"))
+            {
+                string plan = System.IO.File.ReadAllText(currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + "-" + daynum + ".txt");
+                label5.Text = plan;
+                ustawWielk();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)          //gdy chcemy zobaczyć plan na następny dzień tygodnia roboczego
+        {
+            if (daynum == 5)
+            {
+                daynum = 1;
+            }
+            else
+            {
+                daynum++;
+            }
+            label6.Text = nazwaDnia();
+            if (!File.Exists(currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + "-" + daynum + ".txt"))
+            {
+                Aktualizuj();
+            }
+            if (File.Exists(currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + "-" + daynum + ".txt"))
+            {
+                string plan = System.IO.File.ReadAllText(currentPath + @"/pliki/" + settings.IniReadValue("Plan", "kod") + "-" + daynum + ".txt");
+                label5.Text = plan;
+                ustawWielk();
+            }
+        }
+
+        public String nazwaDnia()                       //funkcja zwaracjaca nazwe dnia na podstawie numeru dnia
+        {
+            string dzien = "";
+            if (daynum == 1)
+            {
+                dzien = "Poniedziałek";
+            }
+            else if (daynum == 2)
+            {
+                dzien = "Wtorek";
+            }
+            else if (daynum == 3)
+            {
+                dzien = "Środa";
+            }
+            else if (daynum == 4)
+            {
+                dzien = "Czwartek";
+            }
+            else if (daynum == 5)
+            {
+                dzien = "Piątek";
+            }
+            return (dzien);
+        }
+
+        public void ustawWielk() {
+            short odlewej = 21;
+            short margin = 20;
+
+            int lokacjax = 0;
+            lokacjax = odlewej + label3.Width + margin;
+            button2.Location = new Point(
+                 lokacjax,
+                 button2.Location.Y
+                 );
+            lokacjax = lokacjax + margin + button2.Width + margin;
+            label4.Location = new Point(
+                 lokacjax,
+                 label4.Location.Y
+                 );
+            label5.Location = new Point(
+                 lokacjax,
+                 label5.Location.Y
+                 );
+            label6.Location = new Point(
+                 lokacjax,
+                 label6.Location.Y
+                 );
+            lokacjax = lokacjax + margin + label5.Width + margin;
+            button3.Location = new Point(
+                 lokacjax,
+                 button3.Location.Y
+                 );
+            button1.Location = new Point(
+                 lokacjax,
+                 button1.Location.Y
+                 );
+            lokacjax = lokacjax + button3.Width + odlewej;
+            int lokacjay = 0;
+            if (label3.Height + 41 <= label5.Height + 73)
+            {
+                lokacjay = 73 + label5.Height + 42 + 24;
+
+            }
+            else
+            {
+                lokacjay = 41 + label3.Height + 42 + 24;
+            }
+            this.Size = new Size(lokacjax, lokacjay);
         }
 
 
